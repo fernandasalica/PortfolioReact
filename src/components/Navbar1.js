4013fb7be138fc4478375e8518629251d2e45f71
Your Name <you@example.com>
2023-02-24 19:55:34 -0300

    Defino menu como sticky

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -2,45 +2,11 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navbar1 = () => {
   return (
     <>
-      {/* <Navbar
-        collapseOnSelect
-        expand="lg"
-        className="text-black"
-        sticky="top"
-        style={{ backgroundColor: "#EEEEEE" }}
-      >
-        <Container>
-          <Navbar.Brand>
-            <img src="./img/logoFS.png" width={70} href="#"></img>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className="link menu">
-                Inicio
-              </Nav.Link>
-              <Nav.Link href="#portfolio" className="link menu">
-                Portfolio
-              </Nav.Link>
-              <Nav.Link href="#skills" className="link menu">
-                Conocimientos
-              </Nav.Link>
-              <Nav.Link href="#aboutme" className="link menu">
-                Sobre Mí
-              </Nav.Link>
-              <Nav.Link href="#contact" className="link menu">
-                Contacto
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" sticky="top">
         <Container fluid>
           <Navbar.Brand href="#">
             <img src="./img/logoFS.png" width={70} href="#"></img>
