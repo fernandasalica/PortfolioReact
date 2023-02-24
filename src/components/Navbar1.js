f511cce306e9fd6bcdeb00bb1406c83a8f23dbac
Your Name <you@example.com>
2023-02-24 16:13:19 -0300

    Cambio menu desplegable

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -2,11 +2,12 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Navbar1 = () => {
   return (
     <>
-      <Navbar
+      {/* <Navbar
         collapseOnSelect
         expand="lg"
         className="text-black"
@@ -33,6 +34,33 @@ const Navbar1 = () => {
                 Sobre Mí
               </Nav.Link>
               <Nav.Link href="#contact" className="link menu">
+                Contacto
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> */}
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img src="./img/logoFS.png" width={70} href="#"></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="ms-auto my-2 my-lg-0">
+              <Nav.Link href="#home" className="menu">
+                Inicio
+              </Nav.Link>
+              <Nav.Link href="#portfolio" className="menu">
+                Portfolio
+              </Nav.Link>
+              <Nav.Link href="#skills" className="menu">
+                Conocimientos
+              </Nav.Link>
+              <Nav.Link href="#aboutme" className="menu">
+                Sobre Mí
+              </Nav.Link>
+              <Nav.Link href="#contact" className="menu">
                 Contacto
               </Nav.Link>
             </Nav>
